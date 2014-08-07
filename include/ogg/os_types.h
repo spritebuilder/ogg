15a2b75716e3a4fa2db179c619465b0cb27e5f94
Marc Salem <salem@apportable.com>
2014-08-07 15:58:43 -0700

    Add types for APPORTABLE

--- include/ogg/os_types.h
+++ include/ogg/os_types.h
@@ -138,6 +138,16 @@
    typedef unsigned int ogg_uint32_t;
    typedef long long int ogg_int64_t;
 
+#elif defined(APPORTABLE)
+
+#include <stdint.h>
+
+typedef int16_t ogg_int16_t;
+typedef u_int16_t ogg_uint16_t;
+typedef int32_t ogg_int32_t;
+typedef u_int32_t ogg_uint32_t;
+typedef int64_t ogg_int64_t;
+
 #else
 
 #  include <ogg/config_types.h>
